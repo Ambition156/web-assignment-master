ab3930f2801ea6ec47f15ba46a5532a62b35ac3c
白色衬衫 <1355398343@qq.com>
2024-12-22 21:51:21 +0800

    新增food页面

---
+++ js/food.js
@@ -0,0 +1,124 @@
+// 美女
+const meishi = [
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://img0.baidu.com/it/u=2728335950,595561911&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=1067',
+    tabs: ['鞠婧祎', '明星美女'],
+    _id: '6614f117e0ec199b18e2ea74',
+    score: '4.3',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://img1.baidu.com/it/u=3854902322,2201511351&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=667',
+    tabs: ['鞠婧祎', '明星美女'],
+    _id: '6614f117e0ec199b18e2ea74',
+    score: '4.3',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://img2.baidu.com/it/u=285472252,2008782902&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=667',
+    tabs: ['鞠婧祎', '明星美女', '性感女神'],
+    _id: '6614f117e0ec199b18e2ea73',
+    score: '4.2',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://imgpp.ztupic.com/bup/so/20210716/0866b0d7eb2625f3b31dd08aa22f3922-1.jpg?x-oss-process=image/resize,w_1024/quality,q_85/watermark,image_d2F0ZXIucG5n,type_ZmFuZ3poZW5naGVpdGk,text_5L2c5ZOB57yW5Y%2B3OjIzNDQzOTU%3D,order_0,interval_530,align_1,t_100,g_se,x_10,y_10,color_FFFFFF,size_23,shadow_90',
+    tabs: ['鞠婧祎', '明星美女', '性感女神'],
+    _id: '6614f117e0ec199b18e2ea72',
+    score: '3.7',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.ivsky.com%2Fimg%2Ftupian%2Ft%2F202001%2F31%2Fmeishi-002.jpg&refer=http%3A%2F%2Fimg.ivsky.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1737466943&t=59b1db55bda92295050c5d397f36ecf9',
+    tabs: ['鞠婧祎', '明星美女'],
+    _id: '6614f117e0ec199b18e2ea71',
+    score: '4.4',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://img0.baidu.com/it/u=2331727630,666522004&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=625',
+    tabs: ['鞠婧祎', '明星美女', '性感女神'],
+    _id: '6614f117e0ec199b18e2ea70',
+    score: '4.2',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://img1.baidu.com/it/u=2215039393,2107354898&fm=253&fmt=auto&app=138&f=JPEG?w=270&h=360',
+    tabs: ['鞠婧祎', '明星美女'],
+    _id: '6614f117e0ec199b18e2ea6f',
+    score: '4.2',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://img1.baidu.com/it/u=2173092061,3046749543&fm=253&fmt=auto&app=138&f=JPEG?w=366&h=550',
+    tabs: ['鞠婧祎', '明星美女', '性感女神'],
+    _id: '6614f117e0ec199b18e2ea6e',
+    score: '4.6',
+    nickname: '牵着蜗牛闯红灯',
+  },
+  {
+    description:
+      '鞠婧祎身穿黑色羽翼露肩礼裙，露出雪白的肌肤和优越的肩颈线条。图源：微博@鞠婧祎',
+    classid: '65237031189f860b7613acf4',
+    smallPicurl:
+      'https://img1.baidu.com/it/u=1546870886,2205174630&fm=253&fmt=auto&app=138&f=JPEG?w=375&h=500',
+    tabs: ['鞠婧祎', '明星美女', '性感女神'],
+    _id: '6614f117e0ec199b18e2ea6d',
+    score: '4.4',
+    nickname: '牵着蜗牛闯红灯',
+  }
+]
+
+// 获取图片列表容器
+const imgList = document.querySelector('.imgList')
+
+
+// 初始化渲染
+function render(template, val) {
+  var html = val.reduce((preve, cur) => {
+    return (
+      preve +
+      ` <li class="imgItem">
+          <img src="${cur.smallPicurl}" alt="">
+        </li>`
+    )
+  }, ' ')
+
+  // 渲染小说列表
+  template.innerHTML = html
+}
+
+// 渲染壁纸
+render(imgList, meishi)
